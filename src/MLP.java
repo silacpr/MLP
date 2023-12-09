0103d728ae87e08c5c73fb79b321dabf9ea13359
gretter6 <guillaume.retter5@etu.univ-lorraine.fr>
2023-12-09 23:15:15 +0100

    commentaires + test des resultats ET/OU/XOR

--- src/MLP.java
+++ src/MLP.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 public class MLP  {
     protected double	        fLearningRate = 0.6;
     protected Layer[]		fLayers;
     protected TransferFunction 	fTransferFunction;
 
     /**
-     * @param layers Nb neurones par couches
+     * @param layers Nb neurones par couches ->
+     *               taille du tableau = nb de couches cachées ;
+     *               valeurs des indexs = nombre de neurones dans la couche à l'indice correspondant
      * @param learningRate tx d'apprentissage
      * @param fun Function de transfert
      */
@@ -16,8 +20,11 @@ public class MLP  {
         fLayers = new Layer[layers.length];
         for(int i = 0; i < layers.length; i++) {
             if(i != 0) {
+                // new Layer(nombre de neurons, taille de la couche précédente)
                 fLayers[i] = new Layer(layers[i], layers[i - 1]);
             } else {
+                //couche initiale / première couche
+                //new Layer(nombre de neurones, taille de la couche précédente : ici aucune donc 0)
                 fLayers[i] = new Layer(layers[i], 0);
             }
         }
@@ -30,30 +37,48 @@ public class MLP  {
      * @return résultat de l'exécution
      */
     public double[] execute(double[] input) {
+        // indices de parcours pour les boucles for
         int i, j, k;
+        // valeur calculée de la sortie
         double new_value;
 
+        //output : tableau de double -> taille = nombre de neurones de la dernière couche du réseau
         double output[] = new double[fLayers[fLayers.length - 1].Length];
 
         // input en entrée du réseau
+        // le nombre de neurones de la 1ère couche doit correspondre au nombre de valeurs contenues dans input
+        // exemple : pour 3 valeurs dans input il faut 3 neurones dans la 1ère couche
+        // exemple 2 : si le nombre de valeurs dans input est supérieur au nombre de neurones
+        //              alors toutes les input ne seront pas prises en compte en entrée du réseau
         for(i = 0; i < fLayers[0].Length; i++) {
             fLayers[0].Neurons[i].Value = input[i];
         }
 
         // calculs couches cachées et sortie
+        // on calcule pour toutes les couches cachées sauf la 1ère
+        // pour chaque couche :
         for(k = 1; k < fLayers.length; k++) {
+            // pour tous les neurones dans la couche à l'indice k :
             for(i = 0; i < fLayers[k].Length; i++) {
+                // on initialise la nouvelle valeur à 0.0
                 new_value = 0.0;
+                // pour tous les neurones de la couche précédente :
                 for(j = 0; j < fLayers[k-1].Length; j++)
+                    // on augmente la valeur du neurone actuel avec les (poids * la valeur du neurone de la couche
+                    // précédente à l'indice j)
                     new_value += fLayers[k].Neurons[i].Weights[j] * fLayers[k - 1].Neurons[j].Value;
 
+                //on enlève à la valeur du neurone actuel son Bias (?)
                 new_value -= fLayers[k].Neurons[i].Bias;
+                //on remplace la valeur du neurone actuel par le résultat de son évaluation par la fct de transfert
                 fLayers[k].Neurons[i].Value = fTransferFunction.evaluate(new_value);
             }
         }
 
         // Renvoyer sortie
+        // pour tous les neurones de la dernière couche :
         for(i = 0; i < fLayers[fLayers.length-1].Length; i++) {
+            // on ajoute au tableau ouput la valeur du neurone à l'indice i
             output[i] = fLayers[fLayers.length-1].Neurons[i].Value;
         }
         return output;
@@ -67,14 +92,20 @@ public class MLP  {
      */
 
     public double backPropagate(double[] input, double[] output) {
+        //initialise new_output avec les valeurs de sortie des neurones de la dernière couche pour les valeurs d'input
+        // new_output doit être de la même taille q'output
         double new_output[] = execute(input);
+        //System.out.println(Arrays.toString(new_output));
         double error;
         int i, j, k;
 
 
         // Erreur de sortie
+        // pour tous les neurones de la dernière couche / le nombre de sorties
         for(i = 0; i < fLayers[fLayers.length - 1].Length; i++) {
+            // l'erreur = la valeur de la sortie désirée - la sortie constatée
             error = output[i] - new_output[i];
+            // le Delta de neurone à l'indice i = erreur calculé * l'évaluation de la dérivée
             fLayers[fLayers.length-1].Neurons[i].Delta = error * fTransferFunction.evaluateDer(new_output[i]);
         }
 
--- src/Main.java
+++ src/Main.java
@@ -3,78 +3,124 @@ import java.util.Arrays;
 
 public class Main {
 
+    static final double[][] tableET = {
+            {-1,1,-1},
+            {-1,-1,-1},
+            {1,-1,-1},
+            {1,1,1}
+    };
+    static final double[][] tableOU = {
+            {-1,1,1},
+            {-1,-1,-1},
+            {1,-1,1},
+            {1,1,1}
+    };
+
+    static final double[][] tableXOR = {
+            {-1,1,1},
+            {-1,-1,-1},
+            {1,-1,1},
+            {1,1,-1}
+    };
+
     public static void main(String [] args) {
-        double[][] tableET = {
-                {-1,1,-1},
-                {-1,-1,-1},
-                {1,-1,-1},
-                {1,1,1}
-        };
-        double[][] tableOU = {
-                {-1,1,1},
-                {-1,-1,-1},
-                {1,-1,1},
-                {1,1,1}
-        };
+        System.out.println("Calcul sur la table XOR : ");
+        Main.calcul(tableXOR);
+        System.out.println("\nCalcul sur la table ET : ");
+        Main.calcul(tableET);
+        System.out.println("\nCalcul sur la table OU : ");
+        Main.calcul(tableOU);
+    }
 
-        double[][] tableXOR = {
-                {-1,1,1},
-                {-1,-1,-1},
-                {1,-1,1},
-                {1,1,-1}
-        };
-
-        System.out.println(tableXOR.length);
-        System.out.println(tableXOR[0].length);
+    public static void calcul(double[][] table){
         TransferSigmoide transferSigmoide = new TransferSigmoide();
         TransferTangenteHyperbolique transferTangenteHyperbolique = new TransferTangenteHyperbolique();
-        MLP mlp = new MLP(new int[]{2,4},0.01,transferSigmoide);
 
-        for (int i = 0; i < tableXOR.length; i++) {
-            double[] resultats = mlp.execute(tableXOR[i]);
-            System.out.println(Arrays.toString(resultats));
-        }
+        // Attention : le nombre de neurones de le 1ère couche ne doit pas dépasser le nombre de valeur dans input donc
+        //              ici nos input sont les lignes des tables - la dernière valeur à chaque fois
+        // Attention 2 : le nombre de neurones dans la dernière couche indique le nombre d'output que l'on veut.
+        MLP mlp = new MLP(new int[]{2,1},0.03,transferSigmoide);
 
-
-
-
-
-    }
-
-    public static double signeSomme(double[] perceptron, double[] exemples){
-        double somme = 0;
-        for (int i = 0 ; i < perceptron.length - 1 ; i++) {
-            somme += perceptron[i] * exemples[i];
-        }
-        somme -= perceptron[perceptron.length - 1];
-
-        if (somme < 0) {
-            somme = -1;
-        } else {
-            somme = 1;
-        }
-        return somme;
-    }
-
-    public static double[] appPerceptron(double[] perceptron, double [] exemple,
-                                         double sortie, double pas) {
-	/*
-	   mise a jour des poids du perceptron
-	   suivant la regle d'apprentissage
-	   du perceptron
-	*/
-        double signe = signeSomme(perceptron,exemple);
-        //affichePoids(perceptron);
-        if (signe != exemple[exemple.length-1]) {
-
-            /* poids */
-            for (int i = 0; i < perceptron.length - 1; i++) {
-                perceptron[i] = perceptron[i] + pas * exemple[i] * (sortie - signe);
+        double[] resultats = new double[table.length];
+        int nombre_apprentissage = 100;
+        for (int j = 0; j < nombre_apprentissage; j++) {
+            //System.out.println("\nItération numéro : "+j);
+            for (int i = 0; i < table.length; i++) {
+                double[] input = Arrays.copyOf(table[i],2);
+                double[] out = new double[]{table[i][table[i].length-1]};
+                resultats[i] = mlp.backPropagate(input,out);
             }
-            /* seuil */
-            perceptron[perceptron.length - 1] = perceptron[perceptron.length - 1] + pas * -1 * (sortie - signe);
-
         }
-        return perceptron;
+
+        if (Arrays.deepEquals(table, tableXOR)){
+            boolean[] res = new boolean[resultats.length];
+            for (int i = 0; i < resultats.length; i++) {
+                if (resultats[i] < 1) {
+                    resultats[i] = 1;
+                }else{
+                    resultats[i] = -1;
+                }
+            }
+            //System.out.println(Arrays.toString(resultats));
+
+
+            for (int i = 0; i < resultats.length; i++) {
+                res[i] = table[i][table[i].length-1] == resultats[i];
+            }
+            System.out.println(Arrays.toString(res));
+        }else{
+            // teste du MPL sur la table
+            for (int i = 0; i < table.length; i++) {
+                double[] input = Arrays.copyOf(table[i],2);
+                double[] prediction = mlp.execute(Arrays.copyOf(table[i],2));
+                //System.out.println(Arrays.toString(prediction));
+
+                // Appliquer une fonction de seuil pour obtenir des valeurs binaires
+                for (int j = 0; j < prediction.length; j++) {
+                    prediction[j] = (prediction[j] > 0.1) ? 1.0 : 0.0;
+                }
+
+                System.out.println("Entrée : " + Arrays.toString(input) + ", Prédiction : " + Arrays.toString(prediction));
+            }
+        }
+
+
     }
+
+//    public static double signeSomme(double[] perceptron, double[] exemples){
+//        double somme = 0;
+//        for (int i = 0 ; i < perceptron.length - 1 ; i++) {
+//            somme += perceptron[i] * exemples[i];
+//        }
+//        somme -= perceptron[perceptron.length - 1];
+//
+//        if (somme < 0) {
+//            somme = -1;
+//        } else {
+//            somme = 1;
+//        }
+//        return somme;
+//    }
+//
+//    public static double[] appPerceptron(double[] perceptron, double [] exemple,
+//                                         double sortie, double pas) {
+//	/*
+//	   mise a jour des poids du perceptron
+//	   suivant la regle d'apprentissage
+//	   du perceptron
+//	*/
+//        double signe = signeSomme(perceptron,exemple);
+//        //affichePoids(perceptron);
+//        if (signe != exemple[exemple.length-1]) {
+//
+//            /* poids */
+//            for (int i = 0; i < perceptron.length - 1; i++) {
+//                perceptron[i] = perceptron[i] + pas * exemple[i] * (sortie - signe);
+//            }
+//            /* seuil */
+//            perceptron[perceptron.length - 1] = perceptron[perceptron.length - 1] + pas * -1 * (sortie - signe);
+//
+//        }
+//        return perceptron;
+//    }
 }
